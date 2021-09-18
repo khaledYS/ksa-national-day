fdf8962664b22dd1dc6bf014a21a541c487d6406
AmmarYasser <admin@ammar.awash>
2021-09-18 22:23:37 +0300

    updated : an issue in box shadow for the first container is solved and , i did some organizing for the files

--- main.js
+++
@@ -1,7 +0,0 @@
-// select the container that is inside the first container
-const firstContainer = document.querySelector('body > .container.first > .container')
-
-// giving the first container the class active when click
-firstContainer.onclick = ()=>{
-    firstContainer.classList.toggle('active')
-}
