3fb8ff84645552c44bf3ae03862360605401bd9b
AmmarYasser <admin@ammar.awash>
2021-09-26 09:16:32 +0300

    some thing in line 24 in main.js to write false

--- scripts/main.js
+++ scripts/main.js
@@ -21,5 +21,4 @@ document.addEventListener('DOMContentLoaded', ()=>{
     console.log(document.body.classList.value)
     document.body.classList.remove('loading')
     console.log('Finish loading ..')
-
-})
+}, false)
