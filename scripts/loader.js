4cb90c1adc392d64b1b1f29095e526ee046426b2
AmmarYasser <admin@ammar.awash>
2021-10-02 00:32:22 +0300

    fixing the loading feature

--- scripts/loader.js
+++
@@ -1,6 +0,0 @@
-/* contorling loading page */
-document.addEventListener('DOMContentLoaded', ()=>{
-    console.log(document.body.classList.value)
-    document.body.classList.remove('loading')
-    console.log('Finish loading ..')
-}, false)
